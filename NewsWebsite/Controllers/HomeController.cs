74f587cd784d14e20cd072eb888de34edfac37c1
YStorm5 <panhagt5@gmail.com>
2022-12-01 13:58:23 +0700

    Add Pagination

--- NewsWebsite/Controllers/HomeController.cs
+++ NewsWebsite/Controllers/HomeController.cs
@@ -1,32 +1,64 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using NewsWebsite.Models;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using NewsWebsite.Models;
+using System.Net.Mail;
 
-namespace NewsWebsite.Controllers
+namespace TestingNewAPI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public async Task<IActionResult> Index(int page = 1)
         {
-            _logger = logger;
+            if(page > 20)
+            {
+                return NotFound();
+            }
+            List<News> list = new List<News>();
+            string Baseurl = "http://newsapi-001-site1.htempurl.com/";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(Baseurl);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage res = await client.GetAsync($"api/News/Get?page={page}");
+            if (res.IsSuccessStatusCode)
+            {
+                var Respone = res.Content.ReadAsStringAsync().Result;
+                list = JsonConvert.DeserializeObject<List<News>>(Respone);
+
+            }
+            ViewBag.page = page;
+            return View(list);
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Show(string link)
         {
-            return View();
+            string url = "";
+            if (link.Substring(0, 6) == "/index")
+            {
+                url = $"https://freshnewsasia.com/{link}";
+            }
+            else
+            {
+                url = link;
+            }
+            List<News> list = new List<News>();
+            string Baseurl = "http://newsapi-001-site1.htempurl.com/";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(Baseurl);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage res = await client.GetAsync($"api/News/Show?link={url}");
+            if (res.IsSuccessStatusCode)
+            {
+                var Respone = res.Content.ReadAsStringAsync().Result;
+                list = JsonConvert.DeserializeObject<List<News>>(Respone);
+
+            }
+            return View(list);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
